9d1c77afba884314e8d04e5bad74442751ba4c52
Mujakathali <mujaali481@gmail.com>
2024-06-24 00:50:54 +0530

    routed and updated

--- skint/src/App.js
+++ skint/src/App.js
@@ -1,23 +1,30 @@
-
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Taskboard from './components/TaskBoard';
 
 
+import Dashboard from './components/Dashboard/DashBoard';
 
-
-
-
-
-
+import SideNavbar from './components/Dashboard/sideNavbar.jsx';
+import Taskboard from './components/TaskBoard/TaskBoard.jsx';
+import User2 from './components/userpage/User2.jsx';
 
 function App() {
-  
   return (
-    <div >
- 
-<Taskboard/>
-     
-    </div>
+   
+      <div>
+        <BrowserRouter>
+    
+       
+        <Routes>
+          <Route className="active" path='/' element={<Dashboard/>}></Route>
+          <Route className="active" path='/taskboard' element={<Taskboard/>}></Route>
+          <Route path='/userpage'element={<User2/>}></Route>
+        </Routes>
+       
+        </BrowserRouter>
+      </div>
+    
   );
 }
 
